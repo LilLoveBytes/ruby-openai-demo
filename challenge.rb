cd8a52e014a05a62e19d89087ac3e0e91f1b458d
La Tasha L.  Pollard <128096377+LilLoveBytes@users.noreply.github.com>
2025-01-27 22:02:02 +0000

    challenge 3 complete

--- challenge.rb
+++ challenge.rb
@@ -3,25 +3,26 @@ require "dotenv/load"
 
 client = OpenAI::Client.new(access_token: ENV.fetch("OPEN_API_KEY"))
 user_request = ""
+message_history = []
 
 while user_request.downcase != 'bye'
 
   puts "Hello! How can I help you today?"
   puts "-" * 50
   user_request = gets.chomp
+ 
+  message_history.push({ "role"=> "user", "content"=> user_request})
 
   api_response = client.chat(
     parameters: {
       model: "gpt-3.5-turbo",
-      messages: [
-        {
-          "role"=> "user",
-          "content"=> user_request
-        }
-      ]
+      messages: message_history
     }
   )
-  puts api_response['choices'][0]['message']['content']
+  chat_response = api_response['choices'][0]['message']['content']
+  puts chat_response
 
   puts "-" * 50
+
+  message_history.push({ "role"=> "assistant", "content"=> chat_response})
   end
